881008c6dbc5e9ad47fef40acc4e0b076e5d10a6
Maksym Zyza <mmziza88@gmail.com>
2021-02-21 20:03:01 +0200

    Updates

---
+++ precache-manifest.d6b33472313d15ba778be582c706f5de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa9d83bca726971b2dd7",
+    "url": "/goit-react-hw-01-components_/static/css/main.9432febf.chunk.css"
+  },
+  {
+    "revision": "fa9d83bca726971b2dd7",
+    "url": "/goit-react-hw-01-components_/static/js/main.9a87b065.chunk.js"
+  },
+  {
+    "revision": "174b6f59eddd2051687a",
+    "url": "/goit-react-hw-01-components_/static/js/runtime~main.ef9e161b.js"
+  },
+  {
+    "revision": "30165b12ea936e6395bd",
+    "url": "/goit-react-hw-01-components_/static/css/2.677eacfa.chunk.css"
+  },
+  {
+    "revision": "30165b12ea936e6395bd",
+    "url": "/goit-react-hw-01-components_/static/js/2.c14d38e4.chunk.js"
+  },
+  {
+    "revision": "440b9b231a84584c31de471bd31d0985",
+    "url": "/goit-react-hw-01-components_/static/media/default.440b9b23.jpg"
+  },
+  {
+    "revision": "788d31d96c3c401724c51fc2a3acc050",
+    "url": "/goit-react-hw-01-components_/index.html"
+  }
+];
